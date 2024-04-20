d1de86e9fa371230609de2cadbd2d707b9254b9c
MiguelJaime <migvel.gomero.1901@hotmail.com>
2024-04-20 13:31:35 -0500

    Hasta Models

---
+++ GamarraPlus/Models/DetalleCompra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace GamarraPlus.Models
+{
+    public class DetalleCompra
+    {
+        public int IdDetalleCompra { get; set; }
+        public int IdCompra { get; set; }
+        public int IdProducto { get; set; }
+        public Producto oProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Total { get; set; }
+    }
+}
